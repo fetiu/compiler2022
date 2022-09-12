4cd2b09710f0efb1ca9057077c183932d963d916
Jake Kim <jake@getawair.com>
2022-09-12 12:06:50 +0900

    feat: recursive calculation

--- hw1.c
+++ hw1.c
@@ -1,35 +1,97 @@
 #include <stdio.h>
 
-static int char2int(char c, int *i)
+#define MAX_BUF_SIZE 256
+#define is_number(n) (n >= 0 || n <= 9)
+#define is_lambda(r) (r == NULL)
+
+static int calc_expr(const char *expr);
+
+static int char2int(char c)
 {
     // calc ascii diff
     int diff = c - '0';
-    if (diff < 0 || diff > 9) {
-        return -1;
-    }
-    *i = diff;
-    return 0;
+    return diff;
 }
 
-static int is_eol(int a)
+static char *split(const char *src, char delim, char **rest)
 {
-    return (a == EOF || a == '\r' || a == '\n');
+    static char buf[MAX_BUF_SIZE];
+
+    int pos;
+
+    if (rest) {
+        *rest = buf + pos;
+    }
+    return buf;
+}
+
+static int calc_factor(const char *factor)
+{
+    const char *errmsg;
+
+    // ignore leading spaces
+    while (*factor == ' ') {
+        factor++;
+    }
+
+    if (*factor == '(') {
+        // extract expression inside parenthesis
+        char *expr = split(factor + 1, ')', NULL);
+        if (is_lambda(expr)) {
+            errmsg = "empty expression";
+            goto error;
+        }
+        return calc_expr(expr);
+    }
+
+    int num = char2int(*factor);
+    if (!is_number(num)) {
+        errmsg = "factor not a number";
+        goto error;
+    }
+    return num;
+error:
+    printf("[%s] %s: %s\n", __func__, errmsg, factor);
+    return 0xDEADBEEF;
+}
+
+static int calc_term(const char *term)
+{
+    char *term_rest;
+    char *factor = split(term, '*', &term_rest);
+    if (is_lambda(factor)) {
+        printf("[%s] empty factor in %s\n", __func__, term);
+    }
+    
+    if (is_lambda(term_rest)) {
+        return calc_factor(factor);
+    }
+    return calc_factor(factor) * calc_term(term_rest);
+}
+
+static int calc_expr(const char *expr)
+{
+    char *expr_rest;
+    char *term = split(expr, '+', &expr_rest);
+    if (is_lambda(term)) {
+        printf("[%s] empty term in %s\n", __func__, expr);
+    }
+
+    if (is_lambda(expr_rest)) {
+        return calc_term(term);
+    }
+    return calc_term(term) + calc_expr(expr_rest);
 }
 
 int main(void)
 {
     printf("math expr: ");
-    while (1) {
-        char c = getchar();
-        if (is_eol(c)) {
-            break;
-        }
-        int d;
-        if (char2int(c, &d)) {
-            puts("not num");
-        } else {
-            printf("num: %d\n", d);
-        }
+    char buf[MAX_BUF_SIZE];
+    char *expr = gets(buf);
+    if (!expr) {
+        return -1;
     }
+    int res = calc_expr(expr);
+    printf("%s = %d\n", expr, res);
     return 0;
 }
