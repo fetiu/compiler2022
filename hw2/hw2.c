799e1742d7b3d1354bbe73a8e30bd4405f832bd3
Jake Kim <jake@getawair.com>
2022-09-19 22:20:46 +0900

    feat: print out LR parse actions on recursive-decent parser

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -55,25 +55,20 @@ static void get_token()
         num = 0;
         do {
             num = num * 10 + todigit(ch);
-            putchar(ch);
             ch = getchar();
         } while (isdigit(ch));
         token = NUMBER;
         return;
     } else if (ch == '+') {
-        putchar(ch);
         ch = getchar();
         token = PLUS;
     } else if (ch == '*') {
-        putchar(ch);
         ch = getchar();
         token = STAR;
     } else if (ch == '(') {
-        putchar(ch);
         ch = getchar();
         token = LPAREN;
     } else if (ch == ')') {
-        putchar(ch);
         ch = getchar();
         token = RPAREN;
     } else if (ch == EOF || ch == '\n') {
@@ -88,6 +83,7 @@ int main(void)
 {
     get_token();
     int result = expression();
+    puts("acc");
     if (token != END) {
         error(BADTOKEN);
     } else {
@@ -99,9 +95,12 @@ int main(void)
 static int expression(void)
 {
     int result = term();
+    puts("r2");
     while (token == PLUS) {
+        puts("s6");
         get_token();
         result = result + term();
+        puts("r1");
     }
     return result;
 }
@@ -109,11 +108,19 @@ static int expression(void)
 static int term(void)
 {
     int result = factor();
+    puts("r4");
     while (token == STAR) {
+        puts("s7");
         get_token();
         result = result * factor();
+        puts("r3");
     }
     return result;
+}
+
+static int is_factor(void)
+{
+    return (token == NUMBER || token == LPAREN);
 }
 
 static int factor(void)
@@ -121,12 +128,17 @@ static int factor(void)
     int result;
     if (token == NUMBER) {
         result = num;
+        puts("s5");
         get_token();
+        is_factor() ? error(BADTOKEN) : puts("r6");
     } else if (token == LPAREN) {
+        puts("s4");
         get_token();
         result = expression();
         if (token == RPAREN) {
+            puts("s11");
             get_token();
+            is_factor() ? error(BADTOKEN) : puts("r5");
         } else {
             error(NORPAREN);
         }
