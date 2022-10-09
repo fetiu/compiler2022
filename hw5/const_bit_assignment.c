dd870458152681be13ec3af642f5acb4ab1c595b
Jake Kim <jake@getawair.com>
2022-10-09 20:04:04 +0900

    changed c code example

--- hw5/const_bit_assignment.c
+++ hw5/const_bit_assignment.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
 const a = 1;
 
 struct s {
-int : 64;
+int : 32;
 };
 
 main() {
+    int c;
+    c = a, c = a, c = sizeof(struct s);
     printf("%d\n", a);
     struct s b;
     printf("%u\n", sizeof(b));
