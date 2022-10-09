57afff1901dcf6d0196ac1e7552a8d37fe284113
Jake Kim <jake@getawair.com>
2022-10-09 20:18:26 +0900

    improve code example

--- hw5/const_bit_assignment.c
+++ hw5/const_bit_assignment.c
@@ -4,10 +4,16 @@ struct s {
 int : 32;
 };
 
+extern int printf(const char *fmt, ...);
+extern int printf2(const char **fmt, ...);
+extern int printf2(const char **const fmt, ...);
+extern int printf3(const char *const *fmt, ...);
+extern int printf4(const char *const *const fmt, ...);
+
 main() {
     int c;
     c = a, c = a, c = sizeof(struct s);
     printf("%d\n", a);
     struct s b;
-    printf("%u\n", sizeof(b));
+    printf("%zu\n", sizeof b);
 }
